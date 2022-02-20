d7e6835b0cd1bc7d33e878a28df00f461e5061a1
2020leon <leon2000.tw@gmail.com>
2022-02-20 23:12:47 +0800

    Add kernel-version list sort
    The two files: list_sort.[ch] is copied from linux kernel source code.
    They has been a little modified to meet user mode and the code format
    of the project.

---
+++ list_sort.h
@@ -0,0 +1,19 @@
+// commit: 57fa2369ab17d67e6232f85b868652fbf4407206
+// include/linux/list_sort.h
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_LIST_SORT_H
+#define _LINUX_LIST_SORT_H
+
+#include <linux/types.h>
+
+struct list_head;
+
+typedef int
+    __attribute__((nonnull(2, 3))) (*list_cmp_func_t)(void *,
+                                                      const struct list_head *,
+                                                      const struct list_head *);
+
+__attribute__((nonnull(2, 3))) void list_sort(void *priv,
+                                              struct list_head *head,
+                                              list_cmp_func_t cmp);
+#endif
